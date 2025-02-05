ab2acd31f2ffa84d2bc7346ce30a37c3834a57e2
Pranit Karkera <pranitkarkera99@gmail.com>
2025-02-05 14:08:03 +0530

    added responsivenss to product listing

--- src/pages/shopping-view/ProductListingPage.jsx
+++ src/pages/shopping-view/ProductListingPage.jsx
@@ -138,7 +138,7 @@ const ProductListingPage = () => {
               sortedProducts.map((product) => (
                 <div className="col-md-3 mb-4" key={product.productId}>
                   <div className="card hover-effect position-relative">
-                    <div>
+                    <div className="position-relative">
                       <Link
                         to={`/products/${product.productId}`}
                         style={{ textDecoration: "none" }}
